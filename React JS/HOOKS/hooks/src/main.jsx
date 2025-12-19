fc0c132c405d31345d56a284dd66afb700d68383
Rayane Berrada <153126346+iitsh@users.noreply.github.com>
2025-12-19 09:24:25 +0100

    Update main.jsx

--- React JS/HOOKS/hooks/src/main.jsx
+++ React JS/HOOKS/hooks/src/main.jsx
@@ -1,14 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Compteur from './Exemple1'
-import MonComposant from './Exemple2'
-import Increment from './Exemple3_incrementation'
-import Decrement from './Exemple3_decrementation'
-import Increment_dec_ini from './Exemple3_incrementation'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Increment_dec_ini />
-  </StrictMode>,
-)
