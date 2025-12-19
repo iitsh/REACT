b993ab5af582888a5832a6c99fd0df01b54b6dce
Rayane Berrada <153126346+iitsh@users.noreply.github.com>
2025-12-19 09:22:30 +0100

    Update Exemple1.jsx

--- React JS/HOOKS/hooks/src/Exemple1.jsx
+++ React JS/HOOKS/hooks/src/Exemple1.jsx
@@ -1,16 +1,1 @@
-// const [etat, setEtat] = useState(valeurInitiale);
-import { useState } from "react";
 
-
-const Compteur = () => {
-    const [compte, setCompte] = useState(0); // toujours a l'interieur du composant/fonction
-
-    return(
-        <>
-        <h1>{console.log(compte)}</h1>
-        {console.log(compte+1)}
-        </>
-    )
-
-}
-export default Compteur
