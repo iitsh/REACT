fd034adef7bdba479ed42e7f3c2cbf2b8df0c2a1
Rayane Berrada <153126346+iitsh@users.noreply.github.com>
2025-12-19 09:23:47 +0100

    Remove addition function and component from Exemple2

--- React JS/HOOKS/hooks/src/Exemple2.jsx
+++ React JS/HOOKS/hooks/src/Exemple2.jsx
@@ -1,12 +1,1 @@
-const addition = (a, b) => a + b
-const x = 'toto'
 
-const MonComposant = () => {
-    return(
-        <>
-         <h1>{console.log(addition(3,4))}</h1>         {/*inspecter -> console */}
-        {x} {/* affichage de la variable dans la page*/}
-        </>
-    )
-}
-export default MonComposant
