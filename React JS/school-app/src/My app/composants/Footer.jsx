0dbe815ac8b6eaf8765e4709c974e7f83b192281
Rayane Berrada <153126346+iitsh@users.noreply.github.com>
2025-12-19 09:39:49 +0100

    Delete React JS/school-app/src/My app directory

--- React JS/school-app/src/My app/composants/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <>
-        <footer>
-            <p style={{color:'black', marginLeft:'530px', fontWeight:'bold'}}>&copy; 2025 Mon Application.</p>
-            </footer> </>
-    )
-}
